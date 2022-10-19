11ba1e82115f1e8535bf00c8bf1088b43671ead6
GERTNER <cg0960394@otc.edu>
2022-10-19 09:33:41 -0500

    Added Display All

--- index.php
+++ index.php
@@ -21,5 +21,37 @@
 </head>
 <body>
     
+<?php
+switch($action) {
+    case 'add':
+        $product_key = filter_input(INPUT_POST, 'productkey');
+        $item_qty = filter_input(INPUT_POST, 'itemqty');
+        add_item($product_key, $item_qty);
+        include('cart_view.php');
+        break;
+    case 'update':
+        $new_qty_list = filter_input(INPUT_POST, 'newqty', FILTER_DEFAULT, 
+                                     FILTER_REQUIRE_ARRAY);
+        foreach($new_qty_list as $key => $qty) {
+            if ($_SESSION['cart12'][$key]['qty'] != $qty) {
+                update_item($key, $qty);
+            }
+        }
+        include('cart_view.php');
+        break;
+    case 'show_cart':
+        include('cart_view.php');
+        break;
+    case 'show_add_item':
+        include('add_item_view.php');
+        break;
+    case 'empty_cart':
+        unset($_SESSION['cart12']);
+        include('cart_view.php');
+        break;
+}
+
+
+?>
 </body>
 </html>
--- view/display_all_products.php
+++ view/display_all_products.php
@@ -5,6 +5,7 @@
 // Product Dsc
 // Product Price
 // Product Image
+    echo("<h2> Our Products")
     foreach($item in $products)
     {
         echo("
@@ -13,10 +14,25 @@
             <h4> $item[3] </h4>
             <p> $item[2] </p>
             <img class='itemIcon' src='Images/$item[4]'>
+
+            <form>
+
+
+            </form action='.' method='post'>
+
+            </form>
+            <hr>
         </div>
         ")
     }
 
-
+    echo("<select name='productkey'>");
+    foreach($item in $products) {
+        echo("<option value="$item[0]">
+            <?php echo $item[1]; ?>
+        </option>");
+    }
+    echo("</select> <br> <input type='hidden' name='action' value='add'>");
+    
 
 ?>
