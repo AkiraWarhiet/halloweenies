48dae10d217d9d90dd17e44b26898bcab88addae
GERTNER <cg0960394@otc.edu>
2022-10-19 09:55:50 -0500

    Updated Index and View

--- index.php
+++ index.php
@@ -22,34 +22,15 @@
 <body>
     
 <?php
-switch($action) {
-    case 'add':
-        $product_key = filter_input(INPUT_POST, 'productkey');
-        $item_qty = filter_input(INPUT_POST, 'itemqty');
-        add_item($product_key, $item_qty);
-        include('cart_view.php');
-        break;
-    case 'update':
-        $new_qty_list = filter_input(INPUT_POST, 'newqty', FILTER_DEFAULT, 
-                                     FILTER_REQUIRE_ARRAY);
-        foreach($new_qty_list as $key => $qty) {
-            if ($_SESSION['cart12'][$key]['qty'] != $qty) {
-                update_item($key, $qty);
-            }
-        }
-        include('cart_view.php');
-        break;
-    case 'show_cart':
-        include('cart_view.php');
-        break;
-    case 'show_add_item':
-        include('add_item_view.php');
-        break;
-    case 'empty_cart':
-        unset($_SESSION['cart12']);
-        include('cart_view.php');
-        break;
+
+include("view/display_all_products.php");
+
+$action = filter_input(INPUT_POST, 'action');
+if (!isset($action)) {
+    $action = "showProducts";
 }
+
+nclude("view/display_all_products.php");
 
 
 ?>
--- view/display_all_products.php
+++ view/display_all_products.php
@@ -1,38 +1,41 @@
 
 <?php
-//Product Id,
-//Product name
-// Product Dsc
-// Product Price
-// Product Image
-    echo("<h2> Our Products")
-    foreach($item in $products)
+//Product Id,Product name, Product Dsc, Product Price, Product Image
+    // Header At The Top
+    echo("<h2> Our Products");
+
+    //For Every Item In Our Database
+    foreach($products as $item)
     {
+        // This Displays Its Info
         echo("
         <div class='itemHolder'>
             <h3> $item[1] <h3>
             <h4> $item[3] </h4>
             <p> $item[2] </p>
-            <img class='itemIcon' src='Images/$item[4]'>
-
-            <form>
-
-
-            </form action='.' method='post'>
-
-            </form>
+            <img class='itemIcon' src='data/Images/$item[4]'>
             <hr>
         </div>
-        ")
+        ");
     }
 
+    // Start Of The Select Boxes With The Item Names
     echo("<select name='productkey'>");
-    foreach($item in $products) {
-        echo("<option value="$item[0]">
-            <?php echo $item[1]; ?>
+
+    // For Every Product In The Datbase
+    foreach($products as $item) {
+        // Supply An Option
+        echo("<option value='$item[0]'>
+              $item[1]
         </option>");
     }
-    echo("</select> <br> <input type='hidden' name='action' value='add'>");
-    
+    // This Closes The Select and Adds Our Hidden Input and Starts The Next Select
+    echo("</select> <br> <input type='hidden' name='action' value='add'>  <select name='itemqty'>");
 
+    // Give The Option to buy Up too 10 of the product
+    for($i = 1; $i <= 10; $i++) {
+        echo("<option value='$i'}?> $i </option> ");
+    }
+    // End The Select and Create The Submit Buton
+    echo("</select><br> <input type='submit' value='showCart'>");
 ?>
