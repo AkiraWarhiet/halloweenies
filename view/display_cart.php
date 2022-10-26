f77919310fb24c9b051b05e244706bcdefd2de7d
GERTNER <cg0960394@otc.edu>
2022-10-26 10:07:32 -0500

    Finished Cart Styles: Adding Add Item Styls

--- view/display_cart.php
+++ view/display_cart.php
@@ -1,31 +1,9 @@
 <?php
 
 include "navbar.php";
-// This starts off the html formatting and the select statement
-echo("<form method='post' action='?action=add'> <select name='productkey'>");
-
-    // For Every Product In The Datbase
-    foreach($products as $item) {
-        // Supply An Option
-        echo("<option value='$item[0]'>
-              $item[1]
-        </option>");
-    }
-    // This Closes The Select and Adds Our Hidden Input and Starts The Next Select
-    echo("</select> <select name='itemqty'>");
-
-    for($i = 1; $i <= 10; $i++) {
-        echo("<option value='$i'}?> $i </option> ");
-    }
-    // End The Select and Create The Submit Buton
-    echo("</select> <input type='hidden' name='action' value='add'> <br> <input type='submit' value='Add Item(s)'> </form>");
 
     ?>
     <!-- This gives us a form for the clear so it goes in a post and not a get -->
-    <form action="" method="POST">
-        <input type='hidden' name='action' value='clear'> 
-        <input type='submit' value='Clear Cart'>
-    </form>
     <div id="Cart">
         <?php
 
@@ -51,6 +29,33 @@ echo("<form method='post' action='?action=add'> <select name='productkey'>");
         }
 
         // Define total by adding the subtotals
+        echo("<form method='post' action='?action=add'> <select id='Product' name='productkey'>");
+
+        // For Every Product In The Datbase
+        foreach($products as $item) {
+            // Supply An Option
+            echo("<option value='$item[0]'>
+                $item[1]
+                </option>");
+        }
+
+        // This Closes The Select and Adds Our Hidden Input and Starts The Next Select
+        echo("</select> <select Id='Qty' name='itemqty'>");
+
+        for($i = 1; $i <= 10; $i++) {
+            echo("<option value='$i'}?> $i </option> ");
+        }
+        // End The Select and Create The Submit Buton
+        echo("</select> <input type='hidden' name='action' value='add'> <br> <input id='addItem' type='submit' value='Add Item(s)'> </form>");
+        ?>
+
+         <form action="" method="POST">
+        <input type='hidden' name='action' value='clear'> 
+        <input id="clearCart" type='submit' value='Clear Cart'>
+        </form>
+
+        <?php
+
         $total = getTotal($subtotals);
 
         // Echo total
